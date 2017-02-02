b048cde51bab2943f285174c57c1bcccd665ea45
Alexander Sisyov <sisyov@tutu.ru>
2017-02-02 22:44:06 +0300

    ab_call full commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ab_calc',
+  packages = ['ab_calc'], 
+  version = '0.1',
+  description = 'ab calculation lib',
+  author = 'AS',
+  author_email = 'alexandersisyov@gmail.com',
+  url = 'https://github.com/AlexanderSisev/ab_rep',
+  download_url = 'https://github.com/AlexanderSisev/ab_rep/tarball/0.1',
+  keywords = ['ab', 'ab_test'],
+  classifiers = [],
+)
